22bf489b313f8aa0014b400eb059e60fb70370fd
H. Sabah <sabahhasbi00@gmail.com>
2024-01-08 16:49:25 +0000

    Code fix: type check

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (!startString || !(set instanceof Set) || typeof startString != 'string') return '';
   const arr = Array.from(set).filter((val) => val.startsWith(startString));
   return arr.map((val) => val.replace(startString, '')).join('-');
 }
