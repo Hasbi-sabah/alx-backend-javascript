cbd931c91cca737f622f57ff76f418b8d7628ec7
H. Sabah <sabahhasbi00@gmail.com>
2024-02-26 16:19:24 +0000

    Code fix: readable instead of data

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,9 +2,11 @@ process.stdin.setEncoding('utf8');
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  process.stdout.write('Your name is: ' + data.trim() + '\n');
-  process.stdin.pause();
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
 });
 
 process.stdin.on('end', () => {
