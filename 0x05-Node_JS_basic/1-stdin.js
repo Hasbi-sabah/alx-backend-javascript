21283b410c231ce47bb5a0a7a5625da0c9b7d2b3
H. Sabah <sabahhasbi00@gmail.com>
2024-02-26 16:07:45 +0000

    Code fix: errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
-process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  process.stdout.write('Your name is:', data.trim());
+  process.stdout.write('Your name is: ' + data.trim() + '\n');
   process.stdin.pause();
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
