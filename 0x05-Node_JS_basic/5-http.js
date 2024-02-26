6ece0685d890a10d995fdc2e9100e0b688894d1a
H. Sabah <sabahhasbi00@gmail.com>
2024-02-26 19:15:29 +0000

    Code fix: handle catch

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -37,6 +37,9 @@ const app = http.createServer((req, res) => {
     countStudents(process.argv[2])
       .then((data) => {
         res.end(`This is the list of our students\n${data}`);
+      })
+      .catch((err) => {
+        res.end(err.message);
       });
   }
 });
