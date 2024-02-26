64fc4873d8cdfd9b04206f98d2125e6f754b730f
H. Sabah <sabahhasbi00@gmail.com>
2024-02-26 16:00:26 +0000

    Files added: task 1 Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+process.stdout.write('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write('Your name is:', data.trim());
+  process.stdin.pause();
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing');
+});
