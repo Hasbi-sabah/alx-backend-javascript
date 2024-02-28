8087e71058863f8e312d49e0e3422dbadfb0f3de
H. Sabah <sabahhasbi00@gmail.com>
2024-02-28 16:33:53 +0000

    Update: more tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,24 +3,16 @@ const assert = require('assert');
 const calculateNumber = require("./0-calcul.js");
 
 describe('number calculator', () => {
-    it('0 and 0', () => {
-        res = calculateNumber(0, 0)
-        assert.strictEqual(res, 0)
-    })
-    it('round up pos', () => {
-        res = calculateNumber(1.5, 0)
+    it('first rounded', () => {
+        res = calculateNumber(0.5, 1)
         assert.strictEqual(res, 2)
     })
-    it('round down pos', () => {
-        res = calculateNumber(1.4, 0)
-        assert.strictEqual(res, 1)
+    it('second rounded', () => {
+        res = calculateNumber(1, 0.5)
+        assert.strictEqual(res, 2)
     })
-    it('round up neg', () => {
-        res = calculateNumber(-1.5, 0)
-        assert.strictEqual(res, -1)
-    })
-    it('round down neg', () => {
-        res = calculateNumber(-1.6, 0)
-        assert.strictEqual(res, -2)
+    it('both rounded', () => {
+        res = calculateNumber(1.4, 0.5)
+        assert.strictEqual(res, 2)
     })
 })
