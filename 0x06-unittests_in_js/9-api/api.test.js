7d3b26b88aabec1dab5f3d1cfbc4cdd2517a87b3
H. Sabah <sabahhasbi00@gmail.com>
2024-02-29 21:56:09 +0000

    Code fix: added test case

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,6 +2,13 @@ const { expect } = require('chai')
 const request = require('request')
 
 describe('server test', () => {
+    it('tests the server', (done) => {
+        request.get('http://localhost:7865', (err, res, body) => {
+            expect(res.statusCode).to.equal(200)
+            expect(body).to.equal('Welcome to the payment system')
+            done()
+        })
+    })
     it('tests the server pos', (done) => {
         request.get('http://localhost:7865/cart/12', (err, res, body) => {
             expect(res.statusCode).to.equal(200)
