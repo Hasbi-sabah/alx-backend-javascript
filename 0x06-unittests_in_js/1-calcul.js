b9c872f496c6dd162e9469abaa6a8b043f5eb3c6
H. Sabah <sabahhasbi00@gmail.com>
2024-02-29 17:12:02 +0000

    Code fix: division by 0

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -3,7 +3,7 @@ function calculateNumber(type, a, b) {
         return Math.round(a) + Math.round(b)
     else if (type === 'SUBTRACT')
         return Math.round(a) - Math.round(b)
-    else if (b === 0)
+    else if (Math.round(b) === 0)
         return "Error"
     return Math.round(a) / Math.round(b)
 }
